1bd4e44f73f79afc2fe3f3826ae81b12ae0da081
WearsomeKarma <pahprios@gmail.com>
2026-04-04 19:17:06 -0400

    test: add test suite stubs for entity spawn and get game actions

---
+++ tests/core/source/game_action/core/entity/test_suite_game_action_core_entity_game_action__entity__get.c
@@ -0,0 +1,10 @@
+#include <game_action/core/entity/test_suite_game_action_core_entity_game_action__entity__get.h>
+
+#include <game_action/core/entity/game_action__entity__get.c>
+
+#warning Please make tests for: /home/shalidor/Projects/Lavender/tests/core/source/game_action/core/entity/test_suite_game_action_core_entity_game_action__entity__get.c
+
+// Before writing any tests, please see the README
+// found in ./tests
+
+DEFINE_SUITE(game_action__entity__get, END_TESTS)
---
+++ tests/core/source/game_action/core/entity/test_suite_game_action_core_entity_game_action__entity__spawn.c
@@ -0,0 +1,10 @@
+#include <game_action/core/entity/test_suite_game_action_core_entity_game_action__entity__spawn.h>
+
+#include <game_action/core/entity/game_action__entity__spawn.c>
+
+#warning Please make tests for: /home/shalidor/Projects/Lavender/tests/core/source/game_action/core/entity/test_suite_game_action_core_entity_game_action__entity__spawn.c
+
+// Before writing any tests, please see the README
+// found in ./tests
+
+DEFINE_SUITE(game_action__entity__spawn, END_TESTS)
