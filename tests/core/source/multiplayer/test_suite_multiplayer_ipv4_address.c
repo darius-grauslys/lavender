f119af63440181bd49e142ad6b0cad7c89105744
WearsomeKarma <pahprios@gmail.com>
2026-04-04 21:28:16 -0400

    feat: add test suite skeleton for multiplayer IPv4 address

---
+++ tests/core/source/multiplayer/test_suite_multiplayer_ipv4_address.c
@@ -0,0 +1,10 @@
+#include <multiplayer/test_suite_multiplayer_ipv4_address.h>
+
+#include <multiplayer/ipv4_address.c>
+
+#warning Please make tests for: /home/shalidor/Projects/Lavender/tests/core/source/multiplayer/test_suite_multiplayer_ipv4_address.c
+
+// Before writing any tests, please see the README
+// found in ./tests
+
+DEFINE_SUITE(ipv4_address, END_TESTS)
